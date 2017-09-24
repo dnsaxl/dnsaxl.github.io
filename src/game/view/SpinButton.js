4641bbf607f9b770fc5ed03c2b3c040fb1337bf4
axldns <denis@Deniss-MacBook-Air.local>
2017-09-25 00:20:18 +0200

    button zooming bug fix

--- src/game/view/SpinButton.js
+++ src/game/view/SpinButton.js
@@ -7,38 +7,44 @@ make("SpinButton", function(Button, ClassUtil, msg) {
 		this.on("mouseover", this.onMouseOver);
 		this.on("mouseout", this.onMouseOut);
 		this.on("click", this.onClick);
+		this.zoomed = false;
 	}
 
 	ClassUtil.extend(SpinButton, Button);
 
 	SpinButton.prototype.onMouseOver = function() {
-	    this.scaleX += 0.1;
-	    this.scaleY += 0.1;
+		this.scaleX += 0.1;
+		this.scaleY += 0.1;
+		this.zoomed = true;
 	};
 
 	SpinButton.prototype.onMouseOut = function() {
 		this.scaleX -= 0.1;
 		this.scaleY -= 0.1;
+		this.zoomed = false;
 	};
 
 	SpinButton.prototype.onEnable = function() {
-	    Button.prototype.onEnable.apply(this, arguments);
+		Button.prototype.onEnable.apply(this, arguments);
 		this.alpha = 1;
 	};
 
 	SpinButton.prototype.onDisable = function() {
 		Button.prototype.onDisable.apply(this, arguments);
 		this.alpha = 0.8;
+		if (this.zoomed) {
+			this.onMouseOut();
+		}
 	};
 
 	SpinButton.prototype.onClick = function() {
 		this.enabled = false;
-	    msg.emit(msg.EVENTS.SPIN.BEGIN);
+		msg.emit(msg.EVENTS.SPIN.BEGIN);
 		msg.once(msg.EVENTS.SPIN.STOP, this.onSpinStop);
 	};
 
 	SpinButton.prototype.onSpinStop = function() {
-	    this.enabled = true;
+		this.enabled = true;
 	};
 	return SpinButton;
 });
