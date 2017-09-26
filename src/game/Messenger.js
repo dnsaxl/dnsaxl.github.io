9424d3a7fb635b0e5e9c7b876b6a6636a8a4ee9c
axldns <denis@Deniss-MacBook-Air.local>
2017-09-26 20:43:08 +0200

    before html

--- src/game/Messenger.js
+++ src/game/Messenger.js
@@ -6,7 +6,7 @@ make("Messenger", function(EventEmitter, ClassUtil) {
 		this.PRIORITY_LOWEST = -1000;
 		this.PRIORITY_LOW = -100;
 		this.PRIORITY_HIGH = 100;
-		this.PRIORITY_HIGHEST = 100;
+		this.PRIORITY_HIGHEST = 1000;
 		this.EVENTS = {
 			GAME: {
 				FRAME: "game-frame",
